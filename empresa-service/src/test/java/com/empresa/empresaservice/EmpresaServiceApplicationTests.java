cc7e32d8ec46a12b171cc1647a1bf9102d4fca3b
FranciscoPeluffo <86851752+FranciscoPeluffo@users.noreply.github.com>
2022-04-02 01:22:19 -0300

    Add files via upload

---
+++ empresa-service/src/test/java/com/empresa/empresaservice/EmpresaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.empresaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
